9242732229841f90d64f6b71564a8cf1cfce5fa4
Skvorleksey <skvorleksey@gmail.com>
2021-06-13 23:56:27 +0300

    Pictures gallery interactive was added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const pictures = document.querySelectorAll(".pictire_gallery__image");
 const picturesLinks = document.querySelectorAll(".pictire_gallery__link");
 const picturesGallery = document.querySelector(".pictures_gallery__inner");
 const picturesGalleryWindow = document.querySelector(".pictures_gallery__outer");
+let gallerySlide = 0;
+
+const picturesGalleryArrowLeft = document.querySelector(".pictures_gallery__arrow_left");
+const picturesGalleryArrowRight = document.querySelector(".pictures_gallery__arrow_right");
 
 
 const moveGallery = ()=>{
@@ -21,7 +25,28 @@ const moveGallery = ()=>{
 
     picturesGallery.style.width = `${picturesLinks.length * 100}%`;
     picturesGallery.style.height = `${galleryWindowHeight}px`;
+
+    picturesGallery.style.transform = `translateX(${gallerySlide * galleryWindowWidth * -1}px)`;
 }
 
 moveGallery();
 window.addEventListener('resize', moveGallery);
+picturesGalleryArrowLeft.addEventListener('click',()=> {
+    if (gallerySlide > 0){
+        gallerySlide -= 1;
+    }
+    else{
+        gallerySlide = picturesLinks.length - 1;
+    }
+    moveGallery();
+})
+
+picturesGalleryArrowRight.addEventListener('click',()=> {
+    if (gallerySlide < picturesLinks.length-1){
+        gallerySlide += 1;
+    }
+    else{
+        gallerySlide = 0;
+    }
+    moveGallery();
+})
